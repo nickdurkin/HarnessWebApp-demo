88080a1eeb4f0520f48624e2cfe44082d4e14440
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-07-25 11:44:39 +0200

    New image with Jyoti

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://harness.io/wp-content/uploads/2021/04/jyoti-1.png";
+$auth_message="Jyoti is in your phone :)"; // Landing page
 $my_link=""; // your link
 ?>
