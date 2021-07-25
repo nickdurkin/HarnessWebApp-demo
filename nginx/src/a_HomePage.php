a9e17712313b77fdd7555987ee728f23288f41bd
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-07-25 18:37:17 +0200

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -2,6 +2,6 @@
 //Working on different version
 $auth_version=1; // app Version (1 or 2)
 $auth_icon="";
-$auth_message="Jyoti is in your phone!"; // Landing page
+$auth_message=""; // Landing page
 $my_link=""; // your link
 ?>
