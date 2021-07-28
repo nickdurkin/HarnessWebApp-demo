fddd70238a20ca883ed5dc1f186bb2692eb4295a
nickdurkin <nickdurkin@gmail.com>
2021-07-28 09:25:43 -0700

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.nih.gov/sites/all/themes/nih/images/nih-logo-color.png";
+$auth_message="NIH"; // Landing page
 $my_link=""; // your link
 ?>
